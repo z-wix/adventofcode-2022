c81f06915c9e2cbd9659f88b6b88fe9aeaab8a1d
Zack Wixom <zack.wixom@mx.com>
2022-12-09 16:06:55 -0700

    finished day 5 part 2

--- day5/day.py
+++ day5/day.py
@@ -72,3 +72,51 @@ for x in crate_dict:
         print('[]')
     else: 
         print(crate_dict[x][0])
+
+
+### Part 2 ###
+
+# Create a Crate Dict to show original and ending stacks
+crate_dict = {}
+for i in stack_index:
+    crate_dict['%s' % i] = clean_stack(crates, i)
+
+# Loop thru the steps
+for i in range(steps.shape[0]):
+    # print(f'step: {i}')
+    print(crate_dict)
+    instruction = steps.iloc[i].to_list()
+    print(instruction)
+
+    n_crates = instruction[0]
+    from_stack = instruction[1]
+    to_stack = instruction[2]
+    print(f'Moving: {n_crates} from stack {from_stack} to stack {to_stack}')
+
+    receiving_crate = crate_dict[str(to_stack)]
+    giving_crate = crate_dict[str(from_stack)]
+
+    crate_moving = giving_crate[:n_crates]
+    print(f'{crate_moving} is moving from {giving_crate} to {receiving_crate}')
+
+    crate_moving.reverse()
+
+    for j in crate_moving:
+        print(j)
+        receiving_crate.insert(0, giving_crate.pop(giving_crate.index(j)))
+
+        # Update Stacks after movement
+        updated_from_stack = {str(from_stack): giving_crate}
+        crate_dict.update(updated_from_stack)
+        updated_to_stack = {str(to_stack): receiving_crate}
+        crate_dict.update(updated_to_stack)
+
+    print(f'The Updated Dictionary of Stack is now: {crate_dict}')
+
+# Print out top crate for each stack
+for x in crate_dict:
+    print(x)
+    if len(crate_dict.get(x)) == 0:
+        print('[]')
+    else: 
+        print(crate_dict[x][0])
